06b94cf18a0e518b66b43e4f9a3ce818031052cb
dthorn89 <116689334+dthorn89@users.noreply.github.com>
2023-09-30 21:21:45 +0000

    Include Signal-Server in the main repository

---
+++ communications/signal/Signal-Server/service/src/main/java/org/whispersystems/textsecuregcm/subscriptions/PaymentMethod.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright 2022 Signal Messenger, LLC
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+package org.whispersystems.textsecuregcm.subscriptions;
+
+public enum PaymentMethod {
+  /**
+   * A credit card or debit card, including those from Apple Pay and Google Pay
+   */
+  CARD,
+  /**
+   * A PayPal account
+   */
+  PAYPAL,
+  /**
+   * A SEPA debit account
+   */
+  SEPA_DEBIT,
+}
