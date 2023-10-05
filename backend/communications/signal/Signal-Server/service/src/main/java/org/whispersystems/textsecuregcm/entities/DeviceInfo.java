4049fd7dc735caa62a0c283f675da2380aa0c683
dthorn89 <116689334+dthorn89@users.noreply.github.com>
2023-10-05 03:38:06 +0000

    changed backend to server and frontend to client

--- backend/communications/signal/Signal-Server/service/src/main/java/org/whispersystems/textsecuregcm/entities/DeviceInfo.java
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright 2013-2020 Signal Messenger, LLC
- * SPDX-License-Identifier: AGPL-3.0-only
- */
-
-package org.whispersystems.textsecuregcm.entities;
-
-public record DeviceInfo(long id, String name, long lastSeen, long created) {
-}
